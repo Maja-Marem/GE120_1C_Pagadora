60f546f682ed4522f765df66158dc4a3c03b8313
Maja-Marem <mbpagadora@up.edu.ph>
2024-03-07 15:40:06 +0800

    lectures

---
+++ Exercise 2/Exercise2_PAGADORA.py
@@ -0,0 +1,7 @@
+"""
+Exercise 1
+Maja Marem Jillzam B. Pagadora
+2023-04953
+BS Geodetic Engineering
+"""
+
---
+++ Lectures/Lecture3_Pagadora.py
@@ -0,0 +1,24 @@
+# lists
+
+list = ["mafe", "justin", "mika", "uste"]
+print(list)
+print(list[2])
+print(list[0:2]) 
+# "/:2"
+print(list[2:3]) 
+# "/2:"
+print(list[-1])
+
+list[0] = "chelzy"
+print(list)
+
+# tuple
+tuple_1 = ("maja", "gianna", "jewel")
+
+# immutable
+tuple_1 [0] = "quinmar"
+# valus irreplacable unlike list
+
+# NESTED Lists / also works on tuples
+list1 = [["apple", "bola", "karton"], ["appricot", "banana", "cow"]]
+print(list1[0][2])
