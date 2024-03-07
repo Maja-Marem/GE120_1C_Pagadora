521952997abac297ffa8269815f28c34438321f1
Maja-Marem <mbpagadora@up.edu.ph>
2024-03-07 20:20:05 +0800

    draft 2.0

--- Exercise 2/Exercise2_PAGADORA.py
+++ Exercise 2/Exercise2_PAGADORA.py
@@ -4,19 +4,31 @@ Maja Marem Jillzam B. Pagadora
 2023-04953
 BS Geodetic Engineering
 """
+print()
+print()
+print("LINE DESCRIPTIONS: (closed polygon - line Series)")
 
-# LINE DESCRIPTION
+# constants/lists
 
 Start = 1
 End = 2
 lines=[]
 
 while True:
+
+# line description
+
+    print()
     print()
     print("LINE " + str(Start) + "-" + str(End))
-    dist = (input("Enter Line Distance: "))
-    azs = float(input("Enter Azimuth from the South: ")) % 360
+    print()
 
+    dist = round((float(input("Enter Line Distance: "))),2)
+    azs = round((float(input("Enter Azimuth from the South: ")) % 360),6)
+    print()
+
+# Azimuth from the South in Decimal Degrees to Bearing in DMS
+    
     if azs == 0:
         bearing = "Due South"
     elif azs > 0 and azs < 90:
@@ -54,20 +66,31 @@ while True:
         dms = str(degrees) + "-" + str(minutes) + "-" + str(seconds)
         bearing = "S " + str(dms) + " E"
 
+# line lists input for table
 
-    line = ("LINE " + str(Start) + "-" + str(End) , dist, bearing)
+    line = ("LINE " + str(Start) + "-" + str(End) , str(dist), bearing)
     lines.append(line)
+
+# Continuation / End of Loop
     
-    YN = (input("Add a new line? "))
-    if YN == "yes" or YN.lower == "y":
-        Start = Start + 1
-        End = End + 1
-        continue
+    YN = (input("Add a New line? "))
+    if YN == "yes":
+        typ = (input("Is this a closing line for a polygon ? "))
+        if typ == "yes":
+            Start = Start + 1
+            End =  1
+            continue
+        else :
+            Start = Start + 1
+            End = End + 1
+            continue
     else:
         break
 
+
+print()
 print ("{:-^80}".format("-----------------------"))
 print ("{: ^10} {: ^10} {: ^10} {: ^10} {: ^10} {: ^15} {: ^10}". format("|", "LINE" , "|", "DISTANCE", "|", "BEARING", "|"))
 for line in lines:
     print ("{: ^10} {: ^10} {: ^10} {: ^10} {: ^10} {: ^15} {: ^10}". format("|", line[0], "|", line[1], "|", line[2], "|"))
-print ("{:-^80}".format("----------END----------"))
+print ("{:-^80}".format("--------- END ---------"))
