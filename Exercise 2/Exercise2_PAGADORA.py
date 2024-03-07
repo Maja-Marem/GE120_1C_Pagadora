684a10b75276ebe40276014b908addffcd6dc231
Maja-Marem <mbpagadora@up.edu.ph>
2024-03-07 18:49:11 +0800

    Lecture Notes

--- Exercise 2/Exercise2_PAGADORA.py
+++ Exercise 2/Exercise2_PAGADORA.py
@@ -5,3 +5,67 @@ Maja Marem Jillzam B. Pagadora
 BS Geodetic Engineering
 """
 
+# LINE DESCRIPTION
+
+Start = 1
+lines=[]
+
+while True:
+    print()
+    print("LINE", Start)
+    dist = (input("Enter Line Distance: "))
+    azs = float(input("Enter Azimuth from the South: ")) % 360
+
+    if azs == 0:
+        bearing = "Due South"
+    elif azs > 0 and azs < 90:
+        az = azs
+        degrees = int(az)
+        minutes = int((az - degrees)*60)
+        seconds = round(((((az - degrees)*60)-minutes)*60),2)
+        dms = str(degrees) + "-" + str(minutes) + "-" + str(seconds)
+        bearing = "S " + str(dms) + " W"
+    elif azs == 90:
+        bearing = "Due West"
+    elif azs > 90 and azs < 180:
+        az = 180 - azs
+        degrees = int(az)
+        minutes = int((az - degrees)*60)
+        seconds = round(((((az - degrees)*60)-minutes)*60),2)
+        dms = str(degrees) + "-" + str(minutes) + "-" + str(seconds)
+        bearing = "N " + str(dms) + " W"
+    elif azs == 180:
+        bearing = "Due North"
+    elif azs > 180 and azs < 270:
+        az = azs - 180
+        degrees = int(az)
+        minutes = int((az - degrees)*60)
+        seconds = round(((((az - degrees)*60)-minutes)*60),2)
+        dms = str(degrees) + "-" + str(minutes) + "-" + str(seconds)
+        bearing = "N " + str(dms) + " E"
+    elif azs == 270:
+        bearing = "Due East"
+    else:
+        az = 360 - azs
+        degrees = int(az)
+        minutes = int((az - degrees)*60)
+        seconds = round(((((az - degrees)*60)-minutes)*60),2)
+        dms = str(degrees) + "-" + str(minutes) + "-" + str(seconds)
+        bearing = "S " + str(dms) + " E"
+
+
+    line = ("LINE " , dist, bearing)
+    lines.append(line)
+    
+    YN = (input("Add a new line? "))
+    if YN == "yes" or YN.lower == "y":
+        Start = Start + 1
+        continue
+    else:
+        break
+
+
+print ("{: ^8} {: ^8} {: ^8}". format("LINE" , "DISTANCE", "BEARING"))
+for line in lines:
+    print ("{: ^8} {: ^8} {: ^8}". format(line[0], line[1], line [2]))
+print ("---END---")
--- Lectures/Lecture3_Pagadora.py
+++ Lectures/Lecture3_Pagadora.py
@@ -1,4 +1,4 @@
-# lists
+# lists - acces by index number []
 
 list = ["mafe", "justin", "mika", "uste"]
 print(list)
@@ -12,7 +12,9 @@ print(list[-1])
 list[0] = "chelzy"
 print(list)
 
-# tuple
+
+
+# tuple - access by index number ()
 tuple_1 = ("maja", "gianna", "jewel")
 
 # immutable
@@ -22,3 +24,35 @@ tuple_1 [0] = "quinmar"
 # NESTED Lists / also works on tuples
 list1 = [["apple", "bola", "karton"], ["appricot", "banana", "cow"]]
 print(list1[0][2])
+
+
+
+# dictionary - consists of key value pairs, access by key {}
+dog = {"name": "bogart", "age": "2", "color": "white", "fave_num": "3.14"}
+
+print(dog["name"])
+print(dog.values())
+print(dog.keys())
+
+dog["fave_num"] = 39
+
+print(dog["fave_num"])
+
+# Pseudocode - coding on paper, not actual syntax, logic of the code
+# flow charts - visual representation of the coding logic
+# Algorithms - solving base on steps on solving the problem
+
+
+#  Structured Programming
+
+# Control Structures : 
+"""
+SELECTION - depends on specific values
+ITERATION - 
+SEQUENCE - 
+"""
+
+degrees = int(AzS)
+minutes = int((AzS - degrees)*60)
+seconds = round(((((AzS - degrees)*60)-minutes)*60),2)
+DMS = "S " + "-" + str(degrees) + "-" + str(minutes) + "-" + str(seconds) + " W"
