28a9d5f586a1c3f5bf683523a794cfda03583e22
Maja-Marem <mbpagadora@up.edu.ph>
2024-04-30 15:44:34 +0800

    notes

---
+++ Lectures/lecture7.js
@@ -0,0 +1,99 @@
+// Visualization and GUI Programming
+
+/* 
+I. Web and Mobile Technology Stack
+    a. front-end
+    b. back-end
+    c. data-base
+
+II. Reactive Native
+    a. creating a user interface
+    b. adding functions and ddata control
+    c. Using Expo
+    d. Creating your first .apk file
+
+
+
+* Visualization and GUI Progamming
+
+Graphical User Interfaces (GUI)
+- system of interacctive visual components for computer software
+- objects that convey information and represent actions
+- combination of different parts na pinagsasama 
+
+USe Cases
+- sliders
+- button
+- panels
+- carousels
+- hyperlink
+
+Web and Mobile Texhnologies stack
+- same concept (front end, back end, database)
+
+Full stack development: similar to pass the message
+Data base -> server/backend -> front end
+backend (database and server)
+
+Waiter - front end
+Server - chef
+Data base - supplier
+
+Front end
+- what users see and what they interact with
+- composed of the User interface to give the used a better user experience (UIUX programmer)
+A and B testing (two versions - which is more engaging)
+- GUI elements and basic codes to send the data to and from the server
+- makes it pretty basic codes
+eg. HTML
+
+Back End
+- does the dirty work
+- more of processing
+- how to provide data needed by the user
+- sorting, filtering, processing
+- optimizations of current processes and workflows
+- routes
+
+Database
+- stores all the data
+- stores and organizes all information
+- can perform basic queries (excel sheet filtering)
+- storing and sercurity
+- security of data
+SQL (structured query language ) - seperrated in rows and columns
+eg. MySQL, Microsoft Azure, Aurora
+
+* React Native
+- and open-source UI software framework created by Meta Platforms 
+- use to create native applications for Android, IOS, Web Windows, etc
+(cross platform - pwede in different platforms)
+- ReactJS general term
+- uses java script
+Advantages: cost effective and efficient
+
+Expo
+- makes it even easier
+- built on top of reactive native 
+- remove the compplicated parts of mobile app development 
+
+Basic GUI Elements
+Component 
+- building block of creating UI
+- Reusable piece of code that encapsulates a specific functionalit of UI element (similar to a function)
+- has start and end tag just lik HTML
+- can have properties which describe how a component appears of functions
+View
+- the most fundamental component for building UI
+- Basically a Box
+- container of other components
+- container that supports layout of flexbox, style...
+Text
+- label
+- display information/text
+- static or dynamic
+- nesting, styling, and touch handling
+TextInput
+- inputing text into the keyboard
+
+*/
